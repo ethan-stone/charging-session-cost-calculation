1589fefc3cde06982f480fba589cbd6e885105c3
ethan-stone <ethan.stone9352@gmail.com>
2025-07-07 10:06:30 -0400

    remove unnecessary vitest stuff and run

--- stacks/MyStack.ts
+++
@@ -1,7 +0,0 @@
-import { StackContext, Function } from "sst/constructs";
-
-export function MyStack({ stack }: StackContext) {
-  new Function(stack, "MyFunction", {
-    handler: "packages/functions/src/lambda.handler",
-  });
-}
